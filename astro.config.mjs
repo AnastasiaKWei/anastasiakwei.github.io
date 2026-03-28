5235cbf195d859570f4718ed1bf804bb07f62888
astwei <anastasiawei2025@u.northwestern.edu>
2026-03-28 15:36:41 -0700

    Fix site URL for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astwei.github.io',
+  site: 'https://anastasiakwei.github.io',
   // If deploying to a subpath like https://astwei.github.io/website/
   // uncomment the next line:
   // base: '/website',
